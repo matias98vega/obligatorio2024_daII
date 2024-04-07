f963bb5ed41e86eba923902f6441eaaa4d3f1a81
mp175 <mp_175@hotmail.com>
2024-04-07 17:12:32 -0300

    RED: Prueba para crear edificio con resultado rojo

---
+++ GestionEdificios/GestionEdificios.BusinessLogic.Tests/EdificioTests.cs
@@ -0,0 +1,35 @@
+﻿using GestionEdificios.BusinessLogic.Interfaces;
+using GestionEdificios.DataAccess.Interfaces;
+using GestionEdificios.Domain;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionEdificios.BusinessLogic.Tests
+{
+    [TestClass]
+    public class EdificioTests
+    {
+        private IEdificioLogica eidificioLogica;
+        private Mock<IEdificioLogica> mockRepositorio;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            mockRepositorio = new Mock<IEdificioLogica>(MockBehavior.Strict);
+        }
+
+        [TestMethod]
+        public void TestCrearEdificioOk()
+        {
+            Edificio edificio = new Edificio();
+
+            Edificio edificioCreado = eidificioLogica.Agregar(edificio);
+
+            Assert.AreEqual(edificio.Nombre, edificioCreado.Nombre);
+        }
+    }
+}
